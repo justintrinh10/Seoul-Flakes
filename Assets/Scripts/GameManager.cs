1fe19ce6bb7faff8544f0bff9b5ec0f8b9d0fd7f
justintrinh10 <justintrinh@unr.edu>
2025-11-08 15:11:28 -0800

    Added Game Manager and first assets into scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,34 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private Vector2 customerCoolDownRange = new Vector2(5, 10);
+    private float customerCoolDownTimer;
+    private int numCustomer = 3;
+    private Queue<Customer> customerQueue = new Queue<Customer>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        customerCoolDownTimer = UnityEngine.Random.Range(customerCoolDownRange.x, customerCoolDownRange.y);
     }
 
     // Update is called once per frame
     void Update()
     {
+        customerCoolDownTimer -= Time.deltaTime;
+        if(customerCoolDownTimer <= 0.0f)
+        {
+            Customer newCustomer = new Customer();
+            queueCustomer(newCustomer);
+            customerCoolDownTimer = UnityEngine.Random.Range(customerCoolDownRange.x, customerCoolDownRange.y);
+        }
+    }
 
+    void queueCustomer(Customer newCutomer)
+    {
+        
     }
 }
