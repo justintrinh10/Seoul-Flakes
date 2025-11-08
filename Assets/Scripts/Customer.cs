9e43b27b8827fc057ed8233588f7daddd06823cf
justintrinh10 <justintrinh@unr.edu>
2025-11-08 00:22:12 -0800

    Added appeaarances data to customer class

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -1,16 +1,27 @@
 using System;
+using UnityEngine;
 
 public class Customer
 {
     private Order currentOrder;
+    private string state;
+    private string[] allStates = { "angry", "normal", "happy" };
+    private string color;
+    private string[] allColors = { };
+    private string accessory;
+    private string[] allAccessory = { };
 
     public Customer()
     {
         currentOrder = new Order();
+        state = allStates[1];
+        color = allColors[UnityEngine.Random.Range(0, allColors.Length)];
+        accessory = allAccessory[UnityEngine.Random.Range(0, allAccessory.Length)];
     }
 
-    public void randomCustomerOrder()
+    public void randomCustomer()
     {
+        currentOrder = new Order();
         currentOrder.randomOrder();
     }
 
