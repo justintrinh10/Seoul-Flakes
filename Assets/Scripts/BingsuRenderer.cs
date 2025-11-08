ae851db811b81432598379dcd3372ff5d4d3e3c7
justintrinh10 <justintrinh@unr.edu>
2025-11-08 15:37:23 -0800

    Bingsu Sprite Renderer

--- Assets/Scripts/BingsuRenderer.cs
+++ Assets/Scripts/BingsuRenderer.cs
@@ -2,15 +2,82 @@ using UnityEngine;
 
 public class BingsuRenderer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Bingsu bingsuParent;
+    private SpriteRenderer bingsuSprite;
+
+    [Header("Sprites for Parts")]
+    public Sprite bowlSprite;
+    public Sprite shavedMilkSprite;
+    public Sprite drizzleSprite;
+    public Sprite logoSprite;
+    public Sprite[] baseToppingSprites; // Assign in inspector
+    public Sprite[] toppingSprites;     // Assign in inspector
+
+    private void OnEnable()
     {
-        
+        Bingsu.onAddBowl += ShowBowl;
+        Bingsu.onAddShavedMilk += ShowShavedMilk;
+        Bingsu.onAddDrizzle += ShowDrizzle;
+        Bingsu.onAddLogo += ShowLogo;
+        Bingsu.onAddBaseTopping += ShowBaseTopping;
+        Bingsu.onAddTopping += ShowTopping;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        Bingsu.onAddBowl -= ShowBowl;
+        Bingsu.onAddShavedMilk -= ShowShavedMilk;
+        Bingsu.onAddDrizzle -= ShowDrizzle;
+        Bingsu.onAddLogo -= ShowLogo;
+        Bingsu.onAddBaseTopping -= ShowBaseTopping;
+        Bingsu.onAddTopping -= ShowTopping;
+    }
+
+    private void Start()
+    {
+        bingsuParent = transform.parent.GetComponent<Bingsu>();
+        bingsuSprite = GetComponent<SpriteRenderer>();
+        if (bingsuSprite == null)
+        {
+            bingsuSprite = gameObject.AddComponent<SpriteRenderer>();
+        }
+    }
+
+    // Event callbacks
+    private void ShowBowl() => bingsuSprite.sprite = bowlSprite;
+
+    private void ShowShavedMilk()
+    {
+        // You could layer multiple sprites, or just replace
+        bingsuSprite.sprite = shavedMilkSprite;
+    }
+
+    private void ShowDrizzle() => bingsuSprite.sprite = drizzleSprite;
+
+    private void ShowLogo() => bingsuSprite.sprite = logoSprite;
+
+    private void ShowBaseTopping(string type)
+    {
+        // Find matching sprite by name
+        foreach (var sprite in baseToppingSprites)
+        {
+            if (sprite.name == type)
+            {
+                bingsuSprite.sprite = sprite;
+                break;
+            }
+        }
+    }
+
+    private void ShowTopping(string type)
+    {
+        foreach (var sprite in toppingSprites)
+        {
+            if (sprite.name == type)
+            {
+                bingsuSprite.sprite = sprite;
+                break;
+            }
+        }
     }
 }
