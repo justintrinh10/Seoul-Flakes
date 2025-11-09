4fe028327977d2815463ca0f83fed379e9d4f061
justintrinh10 <justintrinh@unr.edu>
2025-11-08 16:34:19 -0800

    Order renderer

--- Assets/Scripts/OrderRenderer.cs
+++ Assets/Scripts/OrderRenderer.cs
@@ -2,15 +2,65 @@ using UnityEngine;
 
 public class OrderRenderer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Order Parts")]
+    public SpriteRenderer trayRenderer;
+    public SpriteRenderer condensedMilkRenderer;
+
+    [Header("Sprites")]
+    public Sprite traySprite;
+    public Sprite condensedMilkSprite;
+
+    private Order orderParent;
+
+    private void OnEnable()
     {
-        
+        Order.onAddTray += ShowTray;
+        Order.onAddCondensedMilk += ShowCondensedMilk;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        Order.onAddTray -= ShowTray;
+        Order.onAddCondensedMilk -= ShowCondensedMilk;
+    }
+
+    private void Start()
+    {
+        orderParent = GetComponent<Order>();
+        if (orderParent == null)
+        {
+            Debug.LogError("Order component not found on this GameObject!");
+        }
+
+        // Initial render
+        orderParent.OrderSpriteSignals();
+        if (trayRenderer == null)
+        {
+            trayRenderer = gameObject.AddComponent<SpriteRenderer>();
+
+        }
+        if (condensedMilkRenderer == null)
+        {
+            condensedMilkRenderer = gameObject.AddComponent<SpriteRenderer>();
+ 
+        }
+    }
+
+    private void ShowTray()
+    {
+        if (trayRenderer != null)
+        {
+            trayRenderer.sprite = traySprite;
+            trayRenderer.enabled = true;
+        }
+    }
+
+    private void ShowCondensedMilk()
+    {
+        if (condensedMilkRenderer != null)
+        {
+            condensedMilkRenderer.sprite = condensedMilkSprite;
+            condensedMilkRenderer.enabled = true;
+        }
     }
 }
